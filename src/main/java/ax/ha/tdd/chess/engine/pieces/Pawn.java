bbdf587244927243b20c2f4bb641a2916ca6ae61
Capppe <casper.nordstrom@ha.ax>
2024-04-19 14:45:35 +0300

    Started implementation of Pawn

--- src/main/java/ax/ha/tdd/chess/engine/pieces/Pawn.java
+++ src/main/java/ax/ha/tdd/chess/engine/pieces/Pawn.java
@@ -4,6 +4,9 @@ import ax.ha.tdd.chess.engine.Chessboard;
 import ax.ha.tdd.chess.engine.Square;
 import ax.ha.tdd.chess.engine.Color;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pawn extends ChessPieceBase implements ChessPiece{
 
     public Pawn(Color player, Square location) {
@@ -13,6 +16,72 @@ public class Pawn extends ChessPieceBase implements ChessPiece{
     @Override
     public boolean canMove(Chessboard chessboard, Square destination) {
         //TODO here goes move logic for pawns
-        return false;
+        if(chessboard.getPieceAt(destination) != null){
+            return getAvailableTakes(chessboard).contains(destination);
+        }
+        if(!getAvailableMoves().contains(destination)) { return false; }
+
+        return true;
+    }
+
+    private List<Square> getAvailableMoves() {
+        List<Square> moves = new ArrayList<>();
+
+        if(isFirstMove()) {
+            moves.add(new Square(this.location.getX(), this.location.getY() + (this.color == Color.WHITE ? -2 : +2)));
+            moves.add(new Square(this.location.getX(), this.location.getY() + (this.color == Color.WHITE ? -1 : +1)));
+        } else if(this.location.getY() > -1 && this.location.getY() < 8){
+            moves.add(new Square(this.location.getX(), this.location.getY() + (this.color == Color.WHITE ? -1 : +1)));
+        }
+
+        if(canPromote()) {
+            //TODO handle promotion
+            moves.add(new Square(this.location.getX(), this.location.getY() + (this.color == Color.WHITE ? -1 : +1)));
+        }
+
+        return moves;
+    }
+
+    private boolean isFirstMove() {
+        return switch (this.color) {
+            case WHITE -> this.location.getY() == 6;
+            case BLACK -> this.location.getY() == 1;
+            case null -> false;
+        };
+    }
+
+    private boolean canPromote() {
+        return switch (this.color) {
+            case WHITE -> this.location.getY() == 1;
+            case BLACK -> this.location.getY() == 6;
+            case null -> false;
+        };
+    }
+
+    private List<Square> getAvailableTakes(Chessboard chessboard) {
+        List<Square> takes = new ArrayList<>();
+        ChessPiece pieceDiagLeft;
+        ChessPiece pieceDiagRight;
+
+        if(this.color == Color.WHITE) {
+            pieceDiagLeft = chessboard.getPieceAt(new Square(this.location.getX() - 1, this.location.getY() - 1));
+            pieceDiagRight = chessboard.getPieceAt(new Square(this.location.getX() + 1, this.location.getY() - 1));
+        } else {
+            pieceDiagLeft = chessboard.getPieceAt(new Square(this.location.getX() + 1, this.location.getY() + 1));
+            pieceDiagRight = chessboard.getPieceAt(new Square(this.location.getX() - 1, this.location.getY() + 1));
+        }
+
+        if(pieceDiagLeft != null && pieceDiagLeft.getColor() != this.color) {
+            takes.add(pieceDiagLeft.getLocation());
+        }
+        if(pieceDiagRight != null && pieceDiagRight.getColor() != this.color) {
+            takes.add(pieceDiagRight.getLocation());
+        }
+
+        pieceDiagLeft = chessboard.getPieceAt(new Square(this.location.getX()+1, this.location.getY()-1));
+        System.out.println("PieceDiagRight: " + pieceDiagRight);
+        System.out.println("PieceDiagLeft: " + pieceDiagLeft);
+        System.out.println("Available takes: " + takes);
+        return takes;
     }
 }
--- src/test/java/ax/ha/tdd/chess/engine/PawnTests.java
+++ src/test/java/ax/ha/tdd/chess/engine/PawnTests.java
@@ -6,8 +6,7 @@ import ax.ha.tdd.chess.engine.pieces.Pawn;
 import ax.ha.tdd.chess.engine.pieces.PieceType;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PawnTests {
 
@@ -23,6 +22,86 @@ public class PawnTests {
 
         //For debugging, you can print the board to console, or if you want
         //to implement a command line interface for the game
+        System.out.println(new ChessboardWriter().print(chessboard));
+    }
+
+    @Test
+    public void testMovePawnForwardsOneStep() {
+        Chessboard chessboard = new ChessboardImpl();
+        Pawn pawn = new Pawn(Color.WHITE, new Square("e2"));
+        chessboard.addPiece(pawn);
+
+        assertTrue(pawn.canMove(chessboard, new Square("e3")));
+
+        System.out.println(new ChessboardWriter().print(chessboard));
+    }
+
+    @Test
+    public void testMovePawnForwardsTwoStepsFirstMove() {
+        Chessboard chessboard = new ChessboardImpl();
+        Pawn pawn = new Pawn(Color.WHITE, new Square("e2"));
+        chessboard.addPiece(pawn);
+
+        assertTrue(pawn.canMove(chessboard, new Square("e4")));
+
+        System.out.println(new ChessboardWriter().print(chessboard));
+    }
+
+    @Test
+    public void testMovePawnForwardsTwoStepsNotFirstMove() {
+        Chessboard chessboard = new ChessboardImpl();
+        Pawn pawn = new Pawn(Color.WHITE, new Square("e3"));
+        chessboard.addPiece(pawn);
+
+        assertTrue(pawn.canMove(chessboard, new Square("e4")));
+        assertFalse(pawn.canMove(chessboard, new Square("e5")));
+
+        System.out.println(new ChessboardWriter().print(chessboard));
+    }
+
+    @Test
+    public void testMovePawnForwardsToOccupiedSquareIsIllegal() {
+        Chessboard chessboard = new ChessboardImpl();
+        Pawn pawn = new Pawn(Color.WHITE, new Square("e3"));
+        Pawn pawn2 = new Pawn(Color.WHITE, new Square("e4"));
+        chessboard.addPiece(pawn);
+        chessboard.addPiece(pawn2);
+
+        assertFalse(pawn.canMove(chessboard, new Square("e4")));
+
+        System.out.println(new ChessboardWriter().print(chessboard));
+    }
+
+    @Test
+    public void testTakePieceLegalMove() {
+        Chessboard chessboard = new ChessboardImpl();
+        Pawn pawn = new Pawn(Color.WHITE, new Square("b2"));
+        Pawn pawn2 = new Pawn(Color.BLACK, new Square("c3"));
+        chessboard.addPiece(pawn);
+        chessboard.addPiece(pawn2);
+
+        assertTrue(pawn.canMove(chessboard, new Square("c3")));
+
+        Pawn pawn3 = new Pawn(Color.BLACK, new Square("c6"));
+        Pawn pawn4 = new Pawn(Color.WHITE, new Square("d5"));
+        chessboard.addPiece(pawn3);
+        chessboard.addPiece(pawn4);
+
+        assertTrue(pawn3.canMove(chessboard, new Square("d5")));
+
+        System.out.println(new ChessboardWriter().print(chessboard));
+    }
+
+    @Test
+    public void testTakeOwnPieceIllegalMove() {
+        Chessboard chessboard = new ChessboardImpl();
+        Pawn pawn = new Pawn(Color.WHITE, new Square("b2"));
+        Pawn pawn2 = new Pawn(Color.WHITE, new Square("c3"));
+        chessboard.addPiece(pawn);
+        chessboard.addPiece(pawn2);
+
+        assertFalse(pawn.canMove(chessboard, new Square("c3")));
+
         System.out.println(new ChessboardWriter().print(chessboard));
     }
 }
